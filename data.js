dc3c581c134f4fd781b0f004b8b0a358df33a16b
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-09-17 14:54:30 -0500

    MARIA CATAÑO

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salon-ppal",
+      "name": "SALON PPAL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.03723406706315302,
+        "pitch": 0.09775252797602896,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8142355546163191,
+          "pitch": 0.12070097983598416,
+          "rotation": 1.5707963267948966,
+          "target": "0-salon-ppal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-rea-lavacabezas",
+      "name": "área Lavacabezas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5596098552330986,
+        "pitch": 0.008951061582621378,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.351386670380048,
+          "pitch": 0.1930996872536035,
+          "rotation": 4.71238898038469,
+          "target": "0-salon-ppal"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
